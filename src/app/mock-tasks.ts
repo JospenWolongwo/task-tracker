c1716815e53a6878bec8970fa3cf0d3438fbca4f
WOLONGWO POUOKEM JOSPEN <jospenwolongwo@gmail.com>
2023-10-27 16:38:30 +0100

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text:'Doctors appointment',
+        day: 'may 05th at 06:30 pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text:'Meeting at work',
+        day: 'may 06th at 01:30 pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text:'Food stuff arrangement',
+        day: 'may 07th at 08:30 am',
+        reminder: false
+    },
+    
+];
