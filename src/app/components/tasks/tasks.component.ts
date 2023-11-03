9dec4ba2ad57d128bfd2685a60910721370430c5
WOLONGWO POUOKEM JOSPEN <jospenwolongwo@gmail.com>
2023-11-03 11:46:27 +0100

    task-items added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -11,12 +11,23 @@ import { Task } from 'src/app/Task';
 export class TasksComponent implements OnInit{
   constructor(private taskService:TaskService){ }
 
+  
   tasks: Task[] = [];
-
-
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks=>this.tasks = tasks));
+    
   }
+  deleteTask(task: Task){
+    this.taskService.deleteTasks(task).subscribe(()=>this.tasks = (this.tasks.filter(t => t.id !== task.id)));
+  }
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task)=> (this.tasks.push(task)));
+  }
+  
 
 }
